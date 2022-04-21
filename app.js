94de079bfd3c9bb7cdd636608275ae76fcad3b57
EnriqueAguilarP <dialectikos@gmail.com>
2022-04-21 18:59:13 -0500

    metodo delete

--- app.js
+++ app.js
@@ -34,3 +34,10 @@ app.put('/v1/explorers', (req, res) => {
     const requestBody = req.body
     res.status(200).json({message: "Updated!"})
 })
+
+app.delete('/v1/explorers/:id', (req, res) => {
+    console.log(`Api explorers Delete request ${new Date()}`)
+    console.log(`Delete explorer with id ${req.params.id}`)
+    const requestBody = req.body
+    res.status(200).json({message: "Deleted"})
+})
